4fefb3bb513236a413bd408855529247e8bb5e2a
julien-muke <julien.muke@gmail.com>
2023-08-14 17:35:54 +0200

    home page

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,5 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
-// import { dark } from "@clerk/themes";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,7 +8,7 @@ function Topbar() {
     <nav className='topbar'>
       <Link href='/' className='flex items-center gap-4'>
         <Image src='/assets/logo.svg' alt='logo' width={28} height={28} />
-        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>CongoFlow</p>
       </Link>
 
       <div className='flex items-center gap-1'>
@@ -29,7 +29,7 @@ function Topbar() {
 
         <OrganizationSwitcher
           appearance={{
-            // baseTheme: dark,
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
